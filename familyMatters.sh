cdb876843bb34601915971bb96f37a77e9a63e33
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2016-09-15 11:44:00 -0400

    assignment1 1st

---
+++ familyMatters.sh
@@ -0,0 +1,23 @@
+
+mkdir family-matters
+alias proj="cd family-matters"
+mkdir grandpa ma pa
+alias proj="cd grandpa"
+curl -O http://i.giphy.com/l41YjxLE84HoAjxxm.gif
+curl -O http://i.giphy.com/3oz8xPYW0sc64eJEEU.gif
+curl -O http://i.giphy.com/gr3f1WcYyWu76.gif
+
+
+alias proj="cd ma"
+mkdir baby
+alias proj="cd .."
+
+
+touch bubba.txt
+touch elizabeth.txt
+touch chip.txt
+mv -f *.gif pa
+mv -f *.txt ma
+alias proj="cd ma/baby"
+touch googoo.gaga
+mv -f family-matters TIY/sandbox/
